8b4dbce20ecf36e295ed91d6b2d81724fdbe6d03
oleksii-she <94850347+oleksii-she@users.noreply.github.com>
2022-08-19 16:59:10 +0000

    Deploying to gh-pages from @ oleksii-she/goit-js-hw-11@3c8ce208f8b03362da14b056c0ea33d7cd27a7a5 🚀

---
+++ index.5e3d12ff.js
@@ -0,0 +1,2 @@
+fetch("https://restcountries.https://pixabay.com/api/?key=29380167-bc7f7d83fdfa795e7dcbffaab&q=yellow+flower&image_type=photo&orientation=horizontal&safesearch=true/v3.1/name/name").then((e=>{if(console.log(e),404===e.status)throw new Error("Error");return e.json()}));
+//# sourceMappingURL=index.5e3d12ff.js.map
