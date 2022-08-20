da7a56513cc1b91ead42d9d85fb7e23cec11053d
Oleksii Shevchenko <vazerius@gmail.com>
2022-08-20 21:34:46 +0300

    +

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -10,35 +10,22 @@
 //   //     method: 'GET',
 //   //   };
 
-//   const response = await fetch(
-//     `${BASE_URL}?key =${key}&q=${q}&image_type=${imageType}&orientation =${orientation}&safesearch =${safesearch}`
-//   );
-//   const photo = response.json();
-//   return photo;
-// }
+export { pixabayApi };
+const axios = require('axios').default;
 
-// pixabayApi();
+const BASE_URL = 'https://pixabay.com/api/';
+const key = '29380167-bc7f7d83fdfa795e7dcbffaab';
+const q = 'yellow+flower';
+const imageType = 'photo';
+const orientation = 'horizontal';
+const safesearch = 'true';
+const name = 'dog';
 
-function fetchCountries() {
-  const BASE_URL = 'https://pixabay.com/api/';
-  const key = '29380167-bc7f7d83fdfa795e7dcbffaab';
-  const q = 'yellow+flower';
-  const imageType = 'photo';
-  const orientation = 'horizontal';
-  const safesearch = 'true';
-  return fetch(
-    `https://restcountries.${BASE_URL}?key=${key}&q=${q}&image_type=${imageType}&orientation=${orientation}&safesearch=${safesearch}/v3.1/name/name`
-  ).then(res => {
-    console.log(res);
-
-    if (res.status === 404) {
-      throw new Error('Error');
-    } else {
-      return res.json();
-    }
-  });
+function pixabayApi(name) {
+  return axios
+    .get(
+      `https://pixabay.com/api/?image_type=photo&key=29380167-bc7f7d83fdfa795e7dcbffaab&q=${name}&orientation=horizontal&safesearch=true`
+    )
+    .then(res => res)
+    .catch(err => console.log(err));
 }
-fetchCountries();
-// ${name}?fields=name,capital,population,flags,languages
-
-// console.log('heas');
--- src/index.js
+++ src/index.js
@@ -1,1 +1,38 @@
-const axios = require('axios').default;
+import { pixabayApi } from './fetchCountries';
+
+const searchForm = document.querySelector('#search-form');
+const gallery = document.querySelector('.gallery');
+function onSubmitSearchImg(e) {
+  e.preventDefault();
+
+  const searchValue = e.target.elements.searchQuery.value;
+
+  pixabayApi(searchValue).then(data => creatGallaryMarkup(data));
+}
+
+function creatGallaryMarkup({ data: { hits } }) {
+  const markup = hits
+    .map(el => {
+      return `<div class="photo-card">
+    <img src="${el.webformatURL}" alt="${el.tags}" loading="lazy"   width="640"   height="480"/>
+    <div class="info">
+      <p class="info-item">
+        <b>Likes: ${el.likes}</b>
+      </p>
+      <p class="info-item">
+        <b>Views: ${el.views}</b>
+      </p>
+      <p class="info-item">
+        <b>Comments: ${el.comments}</b>
+      </p>
+      <p class="info-item">
+        <b>Downloads: ${el.downloads}</b>
+      </p>
+    </div>
+  </div>`;
+    })
+    .join('');
+
+  gallery.innerHTML = markup;
+}
+searchForm.addEventListener('submit', onSubmitSearchImg);
